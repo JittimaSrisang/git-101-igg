ee967ccedd8f497961f738091428de27e8234f32
JittimaSrisang <37680256+JittimaSrisang@users.noreply.github.com>
2020-09-16 14:06:57 +0700

    createFunction

---
+++ js/function.js
@@ -0,0 +1,26 @@
+// const { close } = require("fs")
+
+// function add(a,b){
+//     return a+b
+// }
+
+// function mul(a,b){
+//     return a*b
+// }
+
+// function searchcustomer(customerName){
+  
+// }
+
+// const add = (a,b) =>{
+//     return a+b
+// }
+// console.log(mul(2,3))
+
+// const add  = (a,b) => a+b
+// console.log(add(2,3))
+
+const add  = (a,b) => {
+    return a+b
+}
+console.log(add(2,3))
