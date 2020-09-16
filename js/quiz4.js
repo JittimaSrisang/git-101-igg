0fe6a47e07bfb0b8814d294024b08546fef132dc
JittimaSrisang <37680256+JittimaSrisang@users.noreply.github.com>
2020-09-16 15:39:55 +0700

    quiz4

---
+++ js/quiz4.js
@@ -0,0 +1,14 @@
+// 1
+var txt = "Hello my name is Joke"
+var sln = txt.length;
+console.log(sln)
+
+// 2
+var str = "Hello my name is Joke nice to meet you!"
+var pos = str.indexOf("Joke");
+console.log(pos)
+
+// 3
+var str = "well open up your mind and see like me"
+var pos = str.search("open");
+console.log(pos)
