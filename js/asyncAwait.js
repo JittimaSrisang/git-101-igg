3063ac18c023e11176fe85d447c6e13f11509e87
JittimaSrisang <37680256+JittimaSrisang@users.noreply.github.com>
2020-09-16 15:52:48 +0700

    asyncAwait

---
+++ js/asyncAwait.js
@@ -0,0 +1,14 @@
+const { resolve } = require("path")
+
+myFirstAsync = () => {
+    return new Promise(resolve => {
+        setTimeout(() =>{
+            resolve('Success');
+        }, 250);
+    })
+}
+msg = async () =>{
+    const successMessage = await myFirstAsync();
+    console.log ('yay!', successMessage);
+}
+msg()
