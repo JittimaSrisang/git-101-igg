5da3b3677a045c8ae7056d7d0c991c1dad4a74a3
JittimaSrisang <37680256+JittimaSrisang@users.noreply.github.com>
2020-09-16 15:21:55 +0700

    quiz3

---
+++ js/quiz3.js
@@ -0,0 +1,14 @@
+const num =  90 ;
+if (num >= 81 && num <= 100){
+    return "A"
+}else if (num >= 71 && num <=80){
+    return "B"
+}else if (num >= 61 && num <=70){
+    return "C"
+}else if (num >= 51 && num <=60){
+    return "D"
+}else {
+    return "F"
+}
+
+console.log(num)
