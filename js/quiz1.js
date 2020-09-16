7a8a08a15a14f679010c5f418329e75f68e3a560
JittimaSrisang <37680256+JittimaSrisang@users.noreply.github.com>
2020-09-16 13:49:47 +0700

    quiz1

---
+++ js/quiz1.js
@@ -0,0 +1,9 @@
+let Profile = {
+    firstname: "Jittima",
+    lastname: "Srisang",
+    age: 21,
+    nickname: "Cake",
+    hobby: "read book",
+    phonenumber: "099-892XXXX"
+}
+console.log(Profile)
