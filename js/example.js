7529e898ebd0dd668eedf075f689a0d823ef3940
JittimaSrisang <37680256+JittimaSrisang@users.noreply.github.com>
2020-09-16 13:37:59 +0700

    edit JS

---
+++ js/example.js
@@ -0,0 +1,20 @@
+// const PI = 3.14
+// console.log("PI: ",PI)
+
+let name = "John"
+name = "Joke"
+
+let test = "hello"
+name = `${test} B`
+
+// array
+let str = ['apple' ,'banana', 'kiwi']
+
+let person = {
+    name: "John",
+    age: 15,
+    gender: {
+        name: "male"
+    }
+}
+console.log(person)
